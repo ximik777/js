3205ac6ea9e6865b7f536a6363fabc2c131b1926
ximik777 <samsungs753>
2013-11-27 15:19:20 +0400

    test

---
+++ index.php
@@ -0,0 +1,31 @@
+<?
+    header('Content-type: text/javascript; charset=utf-8');
+
+    $root = $_SERVER['DOCUMENT_ROOT'];
+    $dir = '/js/';
+    $files = array(
+        'core.js',
+        'events.js',
+        'cookie.js',
+        'ajax.js',
+        'dom.js',
+        'dom.buttons.js',
+        'dom.messageBox.js',
+        'langs.js',
+        'trash.js',
+        'oop.js'
+    );
+
+    ob_start();
+
+    foreach($files as $f)
+    {
+        echo '//'.$f."\r\n";
+        echo file_get_contents($root.$dir.$f);
+    }
+
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    echo $content;
+?>
