4f0f14f587349b8c743bdc4ab8d779dca424f97d
ximik777 <samsungs753>
2013-11-29 15:06:42 +0400

    test

--- js/trash.js
+++ js/trash.js
@@ -1,12 +1,9 @@
-cur = {};
 function showTooltip(el, opts) {
-    //if (!vk.loaded && !opts.noload) return;
-
     if (!el) return;
     if (!el.temphide) {
         el.temphide = function() {
             el.showing = false;
-        }
+        };
         addEvent(el, 'mouseout', el.temphide);
     }
     el.showing = true;
@@ -15,20 +12,15 @@ function showTooltip(el, opts) {
     if (!el.tt) {
         el.tt = 'loadingstat';
     }
-
+    if(!window.cur) window.cur = {};
     cur.cancelTooltip = false;
-
-    //if (opts.stat) stManager.add(opts.stat);
-    // stManager.add(['tooltips.js', 'tooltips.css'], function() {
     if (el.tt == 'loadingstat') el.tt = false;
 
     if (!el.showing || cur.cancelTooltip) return;
-    //_cleanHide(el);
 
     if (!el.tt || !el.tt.el || opts.force) {
         tooltips.create(el, opts);
         if (opts.onCreate) opts.onCreate();
     }
     tooltips.show(el, opts);
-    // });
 }
